ebabcfda0251e1d40e644b2d74e90275d3709e2e
alaunyte <brigita.alaunyte@gmail.com>
2020-08-21 12:42:08 +0300

    Updates

---
+++ precache-manifest.1a6baf84c08fae222e407fb03663450a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cb4dd586d09e7085ec66683f8b9a13",
+    "url": "/simple-image-search-app/index.html"
+  },
+  {
+    "revision": "8913546256eb766f8bd9",
+    "url": "/simple-image-search-app/static/css/main.d31aa71d.chunk.css"
+  },
+  {
+    "revision": "12e8e299af8b28f959f8",
+    "url": "/simple-image-search-app/static/js/2.451714b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-image-search-app/static/js/2.451714b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8913546256eb766f8bd9",
+    "url": "/simple-image-search-app/static/js/main.8a7d4466.chunk.js"
+  },
+  {
+    "revision": "9613795c97464173f354",
+    "url": "/simple-image-search-app/static/js/runtime-main.4c5b4b44.js"
+  },
+  {
+    "revision": "aee4facbeae21b5826bd0040de4c3fd2",
+    "url": "/simple-image-search-app/static/media/loader.aee4facb.gif"
+  }
+]);
